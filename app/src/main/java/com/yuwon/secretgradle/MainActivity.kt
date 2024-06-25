0f21065535beb715b15c852700b9e3604ecb6369
Yuwon Son <gyjrst9102@gmail.com>
2024-06-25 22:43:58 +0900

    Message Changed

--- app/src/main/java/com/yuwon/secretgradle/MainActivity.kt
+++ app/src/main/java/com/yuwon/secretgradle/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colorScheme.background
 				) {
-					Greeting(BuildConfig.apiKey)
+					KeyTesting(BuildConfig.apiKey)
 				}
 			}
 		}
@@ -30,9 +30,9 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun KeyTesting(name: String, modifier: Modifier = Modifier) {
 	Text(
-		text = "Hello $name!",
+		text = "$name",
 		modifier = modifier
 	)
 }
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
 	SecretGradleTheme {
-		Greeting("Android")
+		KeyTesting("TEST KEY")
 	}
 }
